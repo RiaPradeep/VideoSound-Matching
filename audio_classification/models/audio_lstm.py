c62d43c98728d0329bb646dece36464da36439fc
RiaPradeep <ria.pradeep@gmail.com>
2020-12-11 00:19:23 -0800

    Update cnn and audio lstm

--- audio_classification/models/audio_lstm.py
+++ audio_classification/models/audio_lstm.py
@@ -9,22 +9,70 @@ from .VideoEncoders import cnn as vcnn
 class Model(nn.Module):
     def __init__(self, audio_size = (1, 257, 690), video_size=(1, 48, 360, 360),
                     num_classes=2, channel1=2, channel2=64, channel3=128, 
-                    kernel_size=(5, 5), padding=(2, 2), stride=(3, 3), out_dim=128):
+                    kernel_size=(5, 5), padding=(2, 2), stride=(3, 3), out_dim=128,
+                    loss_type="bce"):
         super(Model, self).__init__()
         self.video_enc = vcnn.VideoEnc(video_size=video_size[1:], out_dim=128)
         self.audio_enc = alstm.AudioEnc(audio_size=audio_size[1:], out_dim=128)
-        self.out = nn.Sequential(nn.Linear(out_dim, out_dim))
-        self.linear = nn.Sequential(nn.Linear(256, 1),
-                                    nn.Sigmoid())
 
-    def forward(self, audio1, video):
-        b = audio1.shape[0]
-        audio1_enc = self.audio_enc(audio1)
-        audio1_out = self.out(audio1_enc)
+        self.out = nn.Sequential(nn.ReLU(), 
+                                 nn.Linear(out_dim, out_dim),
+                                 nn.ReLU(),
+                                 nn.Linear(out_dim, out_dim))
+        self.linear = nn.Sequential(nn.Linear(2*out_dim, out_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(out_dim, out_dim//2),
+                                    nn.ReLU(),
+                                    nn.Linear(out_dim//2, 1),
+                                    nn.Sigmoid())
+        self.loss_type = loss_type
+
+    def forward_bce(self, audio, video):
+        audio1_enc = self.audio_enc(audio)
+        audio1_out =  self.out(audio1_enc)
         video_enc = self.video_enc(video)
         video_out = self.out(video_enc)
         output = torch.cat((video_out, audio1_out), 1).to(video_enc.device)
         pred = self.linear(output)
-        #output = (video_out - audio1_out).pow(2)
-        #pred = self.linear(output)
         return pred, None
+    
+    def forward_trip(self, audio, video):
+        audio1_enc = self.audio_enc(audio[0])
+        audio1_out =  self.sigmoid(self.out(audio1_enc))
+        audio2_enc = self.audio_enc(audio[1])
+        audio2_out =  self.sigmoid(self.out(audio2_enc))
+        video_enc = self.video_enc(video)
+        video_out =  self.sigmoid(self.out(video_enc))
+        return audio1_enc, audio2_enc, video_out
+
+    def forward_mult(self, audio, video):
+        audio1_enc = self.audio_enc(audio[0])
+        audio1_out =  self.out(audio1_enc)
+        audio2_enc = self.audio_enc(audio[1])
+        audio2_out =  self.out(audio2_enc)
+        video1_enc = self.video_enc(video[0])
+        video1_out =  self.out(video1_enc)
+        video2_enc = self.video_enc(video[1])
+        video2_out =  self.out(video2_enc)
+        output = torch.cat((video2_out, audio1_out), 1).to(video1_enc.device)
+        pred = self.linear(output)
+        return pred, audio1_out, audio2_out, video1_out, video2_out
+
+    def forward_cos(self, audio, video):
+        audio1_enc = self.audio_enc(audio)
+        audio1_out =  self.out(audio1_enc)
+        video_enc = self.video_enc(video)
+        video_out = self.out(video_enc)
+        output = torch.cat((video_out, audio1_out), 1).to(video_enc.device)
+        pred = self.linear(output)
+        return pred, (audio1_out, video_out)
+
+    def forward(self, audio, video):
+        if self.loss_type == 'bce':
+            return self.forward_bce(audio, video)
+        elif self.loss_type == 'triplet':
+            return self.forward_trip(audio, video)
+        elif self.loss_type == 'multi':
+            return self.forward_mult(audio, video)
+        elif self.loss_type == 'cos':
+            return self.forward_cos(audio, video)
